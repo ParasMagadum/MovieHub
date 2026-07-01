65893ca12b84f1600f16d138e578fedb13ec9d98
ParasMagadum <parasmagadum@gmail.com>
2026-07-01 15:21:21 +0530

    path update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import MainLayout from "../layouts/MainLayout"; // match actual case exactly
+import MainLayout from "../Layouts/MainLayout"; // match actual case exactly
 
 import Home from "../pages/Home/Home";
 import Movies from "../pages/Movies/Movies";
